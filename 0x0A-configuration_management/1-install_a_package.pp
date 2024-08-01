5e610ea72cc82473c8ab3e4be4079997782e8b43
FrancisAmenya <amenyafrancis@gmail.com>
2024-07-31 23:41:42 -0700

    0x0A Configuration change 1a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,11 @@
 #Installs puppet
-
 package { 'python3-pip':
   ensure => installed,
 }
 
 # Install Flask 2.1.0 using pip
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  unless  => '/usr/bin/pip3 show Flask | grep 2.1.0',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
