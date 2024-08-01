31de865aa04120720a04d2668746ddc786dc1e3b
FrancisAmenya <amenyafrancis@gmail.com>
2024-07-31 18:16:31 -0700

    0x0A Configuration anagement fix 1a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,6 +4,14 @@ package { 'python3-pip':
   ensure => installed,
 }
 
+# Install Flask 2.1.0 using pip
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep 2.1.0',
+  require => Package['python3-pip'],
+}
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
