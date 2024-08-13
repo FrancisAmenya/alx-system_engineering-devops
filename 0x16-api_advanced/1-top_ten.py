b67f58604e32e6f39b44eca66f4874d161f9e50a
FrancisAmenya <amenyafrancis@gmail.com>
2024-08-13 15:43:00 -0700

    0x16 api advanced 1 - fix

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+1-top_ten
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyRedditApp/0.1 by YourUsername'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        posts = response.json()['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
