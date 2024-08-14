4bd7b11c0dcb7636643b2ab1304ecf3ce3133d6b
FrancisAmenya <amenyafrancis@gmail.com>
2024-08-13 17:15:00 -0700

    0x16 api advanced 0 - fix 0a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
+
 """
 0-subs
 """
+
 import requests
+from sys import argv
+
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
 
     Returns:
-        int: The number of subscribers, or 0 if the subreddit is invalid.
+        int: The number of subscribers, or 0 if invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'mannedstats'}
@@ -23,5 +28,9 @@ def number_of_subscribers(subreddit):
         data = response.json()
         return data['data']['subscribers']
     else:
-        print(f"Error: Received status code {response.status_code} for subreddit '{subreddit}'")
+        print(f"Er: code {response.status_code},subreddit '{subreddit}'")
         return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(argv[1]))
